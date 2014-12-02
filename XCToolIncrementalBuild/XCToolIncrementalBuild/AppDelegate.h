f9e44577f02499741aa3c184627554db62d4bda5
Mason Glidden <mglidden@yelp.com>
2014-12-02 15:13:57 -0800

    Basic project and workspace

---
+++ XCToolIncrementalBuild/XCToolIncrementalBuild/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCToolIncrementalBuild
+//
+//  Created by Mason Glidden on 12/2/14.
+//  Copyright (c) 2014 Yelp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
